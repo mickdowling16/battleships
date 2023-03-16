387aa507efe6c1ce020828e16df97baec5d0e57f
mickdowling16 <michaeldowling16@gmail.com>
2023-03-16 21:39:32 +0000

    Add print statements and line breaks to run_game function

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ def welcome_message():
     print('Welcome to Battleships...\n')
     print("The aim of the game is to sink the computer's ships before they sink yours. Pick a row and column to make your guess...\n")
     player_name = input("Please enter your name here:\n")
-    print("----------")
+    print("----------\n")
 
 def print_board(board, user):
     print(f"{user} GAME BOARD")
@@ -25,6 +25,7 @@ def print_board(board, user):
 def run_game():
     welcome_message()
     print_board(COMPUTER_BOARD, "COMPUTER")
+    print("----------\n")
     print_board(PLAYER_BOARD, "YOUR")
 
 run_game()
